ee3e740f44691d4242081f0fc6a519f3ea2318f4
Sergey Novozhilov <sergionov@mail.ru>
2021-05-28 19:12:16 +0300

    Modified files

--- dist/common.js
+++ dist/common.js
@@ -15,26 +15,28 @@ var fetch = require('node-fetch'); // const createReadStream = require( 'fs' )
 
 var todo = {
   email: 'sergionov@mail.ru',
-  pass: 12345,
+  pass: 123456,
   save: true,
   csrf_token: '900c48d66f07c601b8d1e98bc50e4a26a1264264e25bcec9ddc839dc9e6aefd6'
 };
-params = JSON.stringify(todo); // console.log(todo)
-
-console.log(JSON.stringify(todo));
-url = 'http://httpbin.org/post'; //'http://localhost:80/ajUser.php'
-
+urls = ['http://localhost:80/ajUser.php', 'http://httpbin.org/post', 'http://localhost:80', 'https://трансплант.net/ajUser.php'];
+url = urls[3];
 method = 'POST';
-params = 'email=sergionov@mail.ru';
+params = 'email=sergionov@mail.ru&pass=12345&save=true';
 headers = {
-  'Content-Type': 'text/html; charset=UTF-8'
-};
+  'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
+}; // params = JSON.stringify(todo)
+// headers = { 'Content-Type': 'application/json' }
+// headers = { 'Content-Type': 'text/html; charset=UTF-8' }
+
 fetch(url, {
-  method: method,
-  body: params,
-  headers: headers
+  method: 'post',
+  headers: headers,
+  body: params
 }).then(function (res) {
   return res.json();
 }).then(function (json) {
   return console.log(json);
+})["catch"](function (err) {
+  return console.log(err);
 });
